b4baa3fee19bb6262bdc579a00ae68528e4d5a4a
Marco Laasonen <stenbiten@yahoo.se>
2026-06-16 13:57:15 +0200

    fix(TollCalculatorService): resolve fee schedule gaps at second boundaries

--- TollCalculator/Services/TollCalculator.cs
+++ TollCalculator/Services/TollCalculator.cs
@@ -16,15 +16,15 @@ namespace TollCalculator.Services
 
         private static readonly (TimeSpan From, TimeSpan To, decimal Fee)[] FeeSchedule =
         {
-            (new TimeSpan(6, 0, 0),  new TimeSpan(6, 29, 0),  MinFee),
-            (new TimeSpan(6, 30, 0), new TimeSpan(6, 59, 0),  13),
-            (new TimeSpan(7, 0, 0),  new TimeSpan(7, 59, 0),  MaxFee),
-            (new TimeSpan(8, 0, 0),  new TimeSpan(8, 29, 0),  13),
-            (new TimeSpan(8, 30, 0), new TimeSpan(14, 59, 0), MinFee),
-            (new TimeSpan(15, 0, 0), new TimeSpan(15, 29, 0), 13),
-            (new TimeSpan(15, 30, 0),new TimeSpan(16, 59, 0), MaxFee),
-            (new TimeSpan(17, 0, 0), new TimeSpan(17, 59, 0), 13),
-            (new TimeSpan(18, 0, 0), new TimeSpan(18, 29, 0), MinFee),
+            (new TimeSpan(6, 0, 0),  new TimeSpan(6, 29, 59),  MinFee),
+            (new TimeSpan(6, 30, 0), new TimeSpan(6, 59, 59),  13),
+            (new TimeSpan(7, 0, 0),  new TimeSpan(7, 59, 59),  MaxFee),
+            (new TimeSpan(8, 0, 0),  new TimeSpan(8, 29, 59),  13),
+            (new TimeSpan(8, 30, 0), new TimeSpan(14, 59, 59), MinFee),
+            (new TimeSpan(15, 0, 0), new TimeSpan(15, 29, 59), 13),
+            (new TimeSpan(15, 30, 0),new TimeSpan(16, 59, 59), MaxFee),
+            (new TimeSpan(17, 0, 0), new TimeSpan(17, 59, 59), 13),
+            (new TimeSpan(18, 0, 0), new TimeSpan(18, 29, 59), MinFee),
         };
 
         /// <summary>
